982aa166adce400d1ad69655201baa8dd216818d
V-augustus <vanshvijain1208@gmail.com>
2025-11-12 02:29:37 +0530

    Update __init__.cpython-310.pyc

--- src/test_craft_run.py
+++ src/test_craft_run.py
@@ -1,11 +1,7 @@
-#test script for craft
-import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from src.detection_craft import run_craft_detection
 
 if __name__ == "__main__":
-    image_path = r"C:\Users\vjvan\OneDrive\Desktop\GitHub\Text-Extraction-Model\images\image1.png"
+    image_path = r"images/image1.png"  # update this path to your image
     result = run_craft_detection(image_path)
-    print("✅ Text detection complete.")
-    print("Detected text boxes:", len(result["boxes"]))
+    print("✅ Detection complete!")
+    print(result)
